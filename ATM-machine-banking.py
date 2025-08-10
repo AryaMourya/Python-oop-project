da286a6493f0fcbf6cda2199165e39819f25a722
Arya Mourya <aryamourya0@gmail.com>
2025-08-10 22:56:50 +0530

    Refactor ATM class: add deposit, withdraw, and check_balance methods; improve PIN verification feedback

--- ATM-machine-banking.py
+++ ATM-machine-banking.py
@@ -30,3 +30,30 @@ class ATM:
           return True
        else:
           print(Color.YELLOW + f"Incorrect PIN.Attempts left: {2- attempt}" + Color.RESET)
+          print(Color.RED + f"Too many failed attempts.Access blocked !" + Color.RESET)
+       return False
+    def deposit(self):
+       amount = input("Enter deposit amount : Rs")
+       if amount.isdigit() and int(amount) > 0:
+          amount = float(amount)
+          self.balance += amount
+          print(Color.GREEN + f"Rs{amount} deposited successfully! New balance: Rs{self.balance}" + Color.RESET)
+       else:
+          print(Color.RED + "Invaild amount!" + Color.RESET)
+
+    def withdraw(self):
+       amount = input("Enter withdrawal amount : Rs")
+       if amount.isdigit() :
+        amount = float(amount)
+        if amount <= 0:
+           print(Color.RED + "Invaild amount!" + Color.RESET)
+        elif amount > self.balance:
+           print(Color.RED + "Invaild amount!" + Color.RESET)
+        else:
+           self.balance -= amount
+           print(Color.GREEN + f"withdraw amount = Rs{amount} . New balance is : Rs{self.balance}" + Color.RESET)
+       else:
+          print(Color.RED + "Invaild input!" + Color.RESET)
+
+    def check_balance(self):
+       print(Color.CYAN + f"Your current balance is : Rs{self.balance}" + Color.RESET)
